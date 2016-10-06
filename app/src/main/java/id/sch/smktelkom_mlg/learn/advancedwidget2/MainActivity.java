30478ad668e188233157a9fbeefc61a63da91aac
Maulana Fadia Hadi <maulanafadia118@gmail.com>
2016-10-06 19:21:45 +0700

    BasicButterKnifeFinal

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/MainActivity.java
@@ -0,0 +1,27 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+@BindView(R.id.editTextNama)
+EditText etNama;
+    @BindView(R.id.textViewHasil)
+    TextView tvHasil;
+    @BindColor(R.color.colorAccent)
+    int colorAccent
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        tvHasil.setTextColor(colorAccent);
+    }
+    @onClick(R.id.buttonProses)
+    public void doProcess()
+    {
+        tvHasil.setText("Nama Anda:"+etNama.getText());
+    }
+}
